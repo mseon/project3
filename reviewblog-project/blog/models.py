44f964576e93184dfee78c8d55ee663969ecc8b1
egg-ghost <57212575+egg-ghost@users.noreply.github.com>
2019-12-08 20:54:22 +0900

    현아

---
+++ reviewblog-project/blog/models.py
@@ -0,0 +1,6 @@
+from django.db import models
+
+# Create your models here.
+class Blog(models.Model):
+	image = models.ImageField(upload_to = 'images/')
+	character = models.CharField(max_length = 300)
